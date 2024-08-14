7e9c4fadb98fe8c7187783eff25eb16f60b7d676
AayushMohan <aayushmohan1702@gmail.com>
2024-08-14 21:58:14 +0530

    chore: Add Breadcrumbs component for header navigation

---
+++ app/doc/[id]/layout.tsx
@@ -0,0 +1,14 @@
+import { auth } from "@clerk/nextjs/server";
+
+const DocLayout = ({
+  children,
+  params: { id },
+}: {
+  children: React.ReactNode;
+  params: { id: string };
+}) => {
+  auth().protect();
+
+  return <div>{children}</div>;
+};
+export default DocLayout;
