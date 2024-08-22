f42b76a4988c6b3379b309e4321703f6e6d60adc
AayushMohan <aayushmohan1702@gmail.com>
2024-08-22 18:13:21 +0530

    chore: Update dependencies and add new package for BlockNote

--- components/Document.tsx
+++ components/Document.tsx
@@ -34,7 +34,7 @@ const Document = ({ id }: { id: string }) => {
   };
 
   return (
-    <div>
+    <div className="flex-1 h-full bg-white p-5">
       <div className="flex max-w-6xl mx-auto justify-between pb-5">
         <form className="flex flex-1 space-x-2" onSubmit={updateTitle}>
           {/* Updated title... */}
